6f5dd2d7b12e11cd0f333156fb70b8bc077c87e5
matthewm00 <mateo.menghini@gmail.com>
2021-10-19 18:25:40 -0300

    irq

--- pre_tpe/RowDaBoat/Kernel/irqDispatcher.c
+++ pre_tpe/RowDaBoat/Kernel/irqDispatcher.c
@@ -2,16 +2,27 @@
 #include <stdint.h>
 
 static void int_20();
+static void int_21();
 
-void irqDispatcher(uint64_t irq) {
-	switch (irq) {
-		case 0:
-			int_20();
-			break;
+void irqDispatcher(uint64_t irq)
+{
+	switch (irq)
+	{
+	case 0:
+		int_20();
+		break;
+	case 1:
+		int_21();
+		break;
 	}
 	return;
 }
 
-void int_20() {
+void int_20()
+{
 	timer_handler();
+}
+void int_21()
+{
+	readKeyboard();
 }
