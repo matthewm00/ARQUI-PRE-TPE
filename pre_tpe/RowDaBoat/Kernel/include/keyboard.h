22a510850e2ea60d3dba428fcb6909405cc71795
matthewm00 <mateo.menghini@gmail.com>
2021-10-22 18:15:03 -0300

    keyboard

--- pre_tpe/RowDaBoat/Kernel/include/keyboard.h
+++ pre_tpe/RowDaBoat/Kernel/include/keyboard.h
@@ -5,21 +5,19 @@
 
 #define KEYS 59
 #define ESC 0x01
-#define DELETE_KEY 0x0E
-#define DELETE_KEY_RELEASED 0xFFFFFFFFFFFFFF8E
 #define R_HEXA_CODE 0x13
 #define L_SHIFT 0x2A
 #define R_SHIFT 0x36
 #define CAPS_LCK 0x3A
 #define CTRL 0x1D
 #define SPACE_BAR 0x39
-#define R_SHIFT_RELEASED 0xFFFFFFFFFFFFFFB6
-#define L_SHIFT_RELEASED 0xFFFFFFFFFFFFFFAA
+#define R_SHIFT_RELEASED 0xB6
+#define L_SHIFT_RELEASED 0xAA
 #define ESC_RELEASED 0x81
 
 void keyboardHandler();
 
-extern unsigned char hasKeyboardKey();
-extern unsigned char readKeyboard();
+extern unsigned int hasKeyboardKey();
+extern unsigned int readKeyboard();
 
 #endif
--- pre_tpe/RowDaBoat/Kernel/keyboardDriver.c
+++ pre_tpe/RowDaBoat/Kernel/keyboardDriver.c
@@ -1,26 +1,17 @@
 #include <keyboard.h>
+#define BUFFER_SIZE 100
 
-char readKeyboard();
-int hasKeyboardKey();
-
-#define BUFF_SIZE 100
-typedef struct Buffer {
-    char buffer[BUFF_SIZE];
-    int start;
-    int size;
-} Buffer;
+char buffer[BUFFER_SIZE] = {0};
+int buff_dim = 0;
 
 // https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html pagina de los scancodes
 static const char keyCodes[KEYS][2] = {
     {0, 0}, {ESC, ESC}, {'1', '!'}, {'2', '@'}, {'3', '#'}, {'4', '$'}, {'5', '%'}, {'6', '^'}, {'7', '&'}, {'8', '*'}, {'9', '('}, {'0', ')'}, {'-', '_'}, {'=', '+'}, {'\b', '\b'}, {'\t', '\t'}, {'q', 'Q'}, {'w', 'W'}, {'e', 'E'}, {'r', 'R'}, {'t', 'T'}, {'y', 'Y'}, {'u', 'U'}, {'i', 'I'}, {'o', 'O'}, {'p', 'P'}, {'[', '{'}, {']', '}'}, {'\n', '\n'}, {CTRL, CTRL}, {'a', 'A'}, {'s', 'S'}, {'d', 'D'}, {'f', 'F'}, {'g', 'G'}, {'h', 'H'}, {'j', 'J'}, {'k', 'K'}, {'l', 'L'}, {';', ':'}, {'\'', '\"'}, {'`', '~'}, {L_SHIFT, L_SHIFT}, {'\\', '|'}, {'z', 'Z'}, {'x', 'X'}, {'c', 'C'}, {'v', 'V'}, {'b', 'B'}, {'n', 'N'}, {'m', 'M'}, {',', '<'}, {'.', '>'}, {'/', '?'}, {R_SHIFT, R_SHIFT}, {0, 0}, {0, 0}, {SPACE_BAR, SPACE_BAR}, {CAPS_LCK, CAPS_LCK}};
 
-char Buffer buffer[BUFF_SIZE] = {0};
-buffer.size = 0;
-buffer.start = 0;
-// char *getKeyboardBuffer()
-// {
-//     return buffer;
-// }
+char *getKeyboardBuffer()
+{
+    return buffer;
+}
 
 int shift = 0;
 int capsLock = 0;
@@ -29,7 +20,7 @@ void keyboardHandler()
 {
     if (hasKeyboardKey())
     {
-        unsigned char key = readKeyboard();
+        unsigned int key = readKeyboard();
         // Key is pressed
         if (key >= ESC && key <= CAPS_LCK)
         {
@@ -39,71 +30,39 @@ void keyboardHandler()
             }
             else if (key == CAPS_LCK)
             {
-                capsLock = 1;
+                capsLock = !capsLock;
             }
 
-            if ((shift || capsLock != 0) && (!(R_SHIFT == key || L_SHIFT == key || R_SHIFT_RELEASED == key || L_SHIFT_RELEASED == key)))
+            if (capsLock && keyCodes[key][0] >= 0)
             {
-                buffer[buff_dim++] = keyCodes[key][1];
-            }
-            else
-            {
-                buffer[buff_dim++] = keyCodes[key][0];
+                if (capsLock)
+                    buffer[buff_dim++] = keyCodes[key][0];
+                else
+                    buffer[buff_dim++] = keyCodes[key][1];
             }
         }
         // Key is released
-        else if (key >= ESC_RELEASED)
+        else if (key == L_SHIFT_RELEASED || key == R_SHIFT_RELEASED)
         {
-            if (key == L_SHIFT_RELEASED || key == R_SHIFT_RELEASED)
-            {
-                shift = 0;
-            }
-            else if (keyCodes[key][0] == 0)
-            {
-                return;
-            }
+            shift = 0;
         }
-        else
-            return;
+        return;
     }
 }
 
-int putChar(char c) {    
-    //Resets buffer position to the start of the array if it's full.
-    if(buffer.size >= BUFFER_SIZE)
-        buffer.start++;
-
-    int offset = (buffer.start + buffer.size) % BUFFER_SIZE;
-	buffer.buffer[offset] = c;
-
-    if(buffer.size < BUFFER_SIZE)
-        buffer.size++;
-    
-    return 0;
+void checkBuffer()
+{
+    if (buff_dim == (BUFFER_SIZE - 1))
+    {
+        cleanBuffer();
+    }
 }
 
-char getChar(int index) {
-    if(buffer.size == 0)
-        return 0;
-
-    char toReturn = buffer[index];
-
-    // Removes read character and increments start position
-    buffer.buffer[index] = 0;
-    buffer.start++;
-    buffers.size--;
-
-    if(buffer.start >= BUFFER_SIZE)
-        buffer.start = 0;
-    
-    return toReturn;
-}
-
-void clearBuffer(int index) {
-    buffer.start = 0;
-    buffer.size = 0;
-
-    for(int i=0; i<BUFFER_SIZE; i++) {
-        buffer.buffer[i] = 0; 
+void cleanBuffer()
+{
+    buff_dim = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        buffer[i] = 0;
     }
 }
