c846fc417b974a4653663ac1633ed518c9060684
matthewm00 <mateo.menghini@gmail.com>
2022-05-22 19:41:14 -0300

    Co-authored-by: Santiago A. Larroudé Álvarez <SSanti32@users.noreply.github.com> Co-authored-by: NeksHinto <nhinojo@itba.edu.ar>
    prints

--- Kernel/include/prints.h
+++ Kernel/include/prints.h
@@ -3,10 +3,16 @@
 
 #include <colors.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 void printIntDec(uint64_t num);
 void printIntHex(uint64_t num);
 void printIntBin(uint64_t num);
+int strlen(char *str);
 void printLine();
+void strcpy(char dest[], const char source[]);
 uint64_t uintToBase(uint64_t value, char *buffer, uint32_t base);
+int printf(char const *fmt, ...);
+void putChar(char c);
+char *intToStr(int value, char *buffer, int base);
 #endif
--- Kernel/libraries/prints.c
+++ Kernel/libraries/prints.c
@@ -18,6 +18,24 @@ static int strlen(char *str)
 	return size;
 }
 
+void strcpy(char dest[], const char source[])
+{
+	int i = 0;
+	while (1)
+	{
+		dest[i] = source[i];
+
+		if (dest[i] == 0)
+		{
+			break;
+		}
+
+		i++;
+	}
+}
+
+static int scan(char const *fmt, va_list arg);
+
 // FUNCION DE CATEDRA
 uint64_t uintToBase(uint64_t value, char *buffer, uint32_t base)
 {
@@ -71,3 +89,148 @@ void printIntBin(uint64_t num)
 	uintToBase(num, buffer, 2);
 	sys_write(buffer, strlen(buffer), BLACK, WHITE);
 }
+
+int printf(char const *fmt, ...)
+{
+	va_list arguments;
+	int length;
+
+	va_start(arguments, fmt);
+	length = scan(fmt, arguments);
+	va_end(arguments);
+	return length;
+}
+
+void putChar(char c)
+{
+	sys_write(&c, 1, BLACK, WHITE);
+}
+
+void printString(char *string)
+{
+	sys_write(string, strlen(string), BLACK, WHITE);
+}
+
+static int scan(char const *fmt, va_list arg)
+{
+	int int_temp;
+	char char_temp;
+	char *string_temp;
+
+	char ch;
+	int length = 0;
+
+	char buffer[512];
+
+	while ((ch = *fmt++))
+	{
+		if ('%' == ch)
+		{
+			switch (ch = *fmt++)
+			{
+			case '%':
+				putChar('%');
+				length++;
+				break;
+			case 'c':
+				char_temp = va_arg(arg, int);
+				putChar(char_temp);
+				length++;
+				break;
+			case 's':
+				string_temp = va_arg(arg, char *);
+				printString(string_temp);
+				length += strlen(string_temp);
+				break;
+			case 'd':
+				int_temp = va_arg(arg, int);
+				intToStr(int_temp, buffer, 10);
+				printString(buffer);
+				length += strlen(buffer);
+				break;
+			case 'x':
+				int_temp = va_arg(arg, int);
+				intToStr(int_temp, buffer, 16);
+				printString("0x");
+				printString(buffer);
+				length += strlen(buffer);
+				break;
+			}
+		}
+		else
+		{
+			putChar(ch);
+			length++;
+		}
+	}
+	return length;
+}
+
+// https://www.techiedelight.com/implement-itoa-function-in-c/
+
+// Iterative function to implement itoa() function in C
+char *intToStr(int value, char *buffer, int base)
+{
+	// invalid input
+	if (base < 2 || base > 32)
+	{
+		return buffer;
+	}
+
+	// consider the absolute value of the number
+	int n = abs(value);
+
+	int i = 0;
+	while (n)
+	{
+		int r = n % base;
+
+		if (r >= 10)
+		{
+			buffer[i++] = 65 + (r - 10);
+		}
+		else
+		{
+			buffer[i++] = 48 + r;
+		}
+
+		n = n / base;
+	}
+
+	// if the number is 0
+	if (i == 0)
+	{
+		buffer[i++] = '0';
+	}
+
+	// If the base is 10 and the value is negative, the resulting string
+	// is preceded with a minus sign (-)
+	// With any other base, value is always considered unsigned
+	if (value < 0 && base == 10)
+	{
+		buffer[i++] = '-';
+	}
+
+	buffer[i] = '\0'; // null terminate string
+
+	// reverse the string and return it
+	return reverse(buffer, 0, i - 1);
+}
+
+// function to reverse buffer[i..j]
+char *reverse(char *buffer, int i, int j)
+{
+	while (i < j)
+		swap(&buffer[i++], &buffer[j--]);
+
+	return buffer;
+}
+
+void swap(char *x, char *y)
+{
+	char t = *x;
+	*x = *y;
+	*y = t;
+}
+
+int abs(int num) { return num < 0 ? -num : num; }
