afd7fbf74bb28463ba1718adf18a9a55846a7815
matthewm00 <mateo.menghini@gmail.com>
2022-05-23 13:13:31 -0300

    syscalls development stage

--- Userland/SampleCodeModule/include/syscalls.h
+++ Userland/SampleCodeModule/include/syscalls.h
@@ -4,22 +4,85 @@
 #include <stdint.h>
 #include <colors.h>
 
-#define SYS_RTC_ID 1
-#define SYS_READ_ID 2
-#define SYS_WRITE_ID 3
-#define SYS_INFOREG_ID 4
-#define SYS_PRINTMEM_ID 5
-#define SYS_CLEAR_ID 6
-#define SYS_EXIT_ID 7
+#define FOREGROUND 1
+#define BACKGROUND 0
+typedef enum
+{
+    READY,
+    BLOCKED,
+    TERMINATED
+} t_state;
 
-#define SYS_GAMES_ID 8
-#define SYS_TIME_ID 9
+typedef enum
+{
+    SYS_RTC_ID,
+    SYS_READ_ID,
+    SYS_WRITE_ID,
+    SYS_INFOREG_ID,
+    SYS_PRINTMEM_ID,
+    SYS_CLEAR_ID,
+    SYS_EXIT_ID,
 
-#define SYS_CURSOR_ID 10
-#define SYS_CURSORSTATE_ID 11
-#define SYS_STOPWATCH_ID 12
-#define SYS_STOPWATCHTICKS_ID 13
+    SYS_GAMES_ID,
+    SYS_TIME_ID,
+
+    SYS_CURSOR_ID,
+    SYS_CURSORSTATE_ID,
+    SYS_STOPWATCH_ID,
+    SYS_STOPWATCHTICKS_ID,
+
+    SYS_MALLOC_ID,
+    SYS_FREE_ID,
+    SYS_MEMORY_STATUS_ID,
+    SYS_NEW_PROCESS_ID,
+    SYS_KILL_PROCESS_ID,
+    SYS_PROCESS_PID_ID,
+    SYS_PROCESS_STATUS_ID,
+    SYS_BLOCK_PROCESS_ID,
+    SYS_CHANGE_PROCESS_STATE_ID,
+    SYS_YIELD_ID,
+    SYS_SET_PRIORITY_ID,
+    SYS_UNBLOCK_PROCESS_ID,
+    SYS_SEM_OPEN_ID,
+    SYS_SEM_POST_ID,
+    SYS_SEM_WAIT_ID,
+    SYS_SEM_CLOSE_ID,
+    SYS_SEM_STATUS_ID,
+    SYS_SEC_ELAPSED_ID,
+    SYS_PIPE_STATUS_ID,
+    SYS_PIPE_OPEN_ID,
+    SYS_PIPE_CLOSE_ID,
+    SYS_PIPE_WRITE_ID,
+    SYS_PIPE_READ_ID,
+    SYS_WAIT_ID
+} t_syscall_id;
 
 uint64_t _syscall(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+
+int newProcess(void (*entryPoint)(int, char **), int argc, char **argv,
+               int foreground, int *fd);
+int killProcess(uint64_t pid);
+int getProcessPID();
+void processStatus();
+int blockProcess(uint64_t pid);
+int setState(uint64_t pid, t_state newState);
+void yield();
+void setPriority(uint64_t pid, int newPriority);
+int unblockProcess(uint64_t pid);
+void *malloc(uint64_t size);
+void free(void *address);
+void memStatus();
+int semOpen(uint32_t id, uint64_t initialValue);
+int semWait(uint32_t id);
+int semClose(uint32_t id);
+void semStatus();
+int semPost(uint32_t id);
+int getSecondsElapsed();
+void pipeStatus();
+int pipeOpen(int pipeID);
+int pipeClose(int pipeID);
+int pipeWrite(int pipeID, char *str);
+int pipeRead(int pipeID);
+void wait(int pipeID);
 
 #endif
---
+++ Userland/SampleCodeModule/libraries/syscalls.c
@@ -0,0 +1,104 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include <syscalls.h>
+
+int newProcess(void (*entryPoint)(int, char **), int argc, char **argv,
+               int foreground)
+{
+  return _syscall(SYS_NEW_PROCESS_ID, (uint64_t)entryPoint, argc,
+                  (uint64_t)argv, (uint64_t)foreground);
+}
+
+int killProcess(uint64_t pid)
+{
+  return _syscall(SYS_KILL_PROCESS_ID, pid, 0, 0, 0);
+}
+
+int getProcessPID() { return _syscall(SYS_PROCESS_PID_ID, 0, 0, 0, 0); }
+
+void processStatus() { (void)_syscall(SYS_PROCESS_STATUS_ID, 0, 0, 0, 0); }
+
+int blockProcess(uint64_t pid)
+{
+  return _syscall(SYS_BLOCK_PROCESS_ID, pid, 0, 0, 0);
+}
+
+int unblockProcess(uint64_t pid)
+{
+  return _syscall(SYS_UNBLOCK_PROCESS_ID, pid, 0, 0, 0);
+}
+
+int setState(uint64_t pid, t_state newState)
+{
+  return _syscall(SYS_CHANGE_PROCESS_STATE_ID, pid, (uint64_t)newState, 0, 0);
+}
+
+void yield() { (void)_syscall(SYS_YIELD_ID, 0, 0, 0, 0); }
+
+void setPriority(uint64_t pid, int newPriority)
+{
+  (void)_syscall(SYS_SET_PRIORITY_ID, pid, newPriority, 0, 0);
+}
+
+void *malloc(uint64_t size)
+{
+  return (void *)_syscall(SYS_MALLOC_ID, size, 0, 0, 0);
+}
+
+void free(void *address)
+{
+  (void)_syscall(SYS_FREE_ID, (uint64_t)address, 0, 0, 0);
+}
+
+int semOpen(uint32_t id, uint64_t initialValue)
+{
+  return _syscall(SYS_SEM_OPEN_ID, (uint64_t)id, initialValue, 0, 0);
+}
+
+int semWait(uint32_t id)
+{
+  return _syscall(SYS_SEM_WAIT_ID, (uint64_t)id, 0, 0, 0);
+}
+
+int semPost(uint32_t id)
+{
+  return _syscall(SYS_SEM_POST_ID, (uint64_t)id, 0, 0, 0);
+}
+
+int semClose(uint32_t id)
+{
+  return _syscall(SYS_SEM_CLOSE_ID, (uint64_t)id, 0, 0, 0);
+}
+
+void semStatus() { (void)_syscall(SYS_SEM_STATUS_ID, 0, 0, 0, 0); }
+
+void memStatus() { (void)_syscall(SYS_MEMORY_STATUS_ID, 0, 0, 0, 0); }
+
+int getSecondsElapsed() { return _syscall(SYS_SEC_ELAPSED_ID, 0, 0, 0, 0); }
+
+void pipeStatus() { (void)_syscall(SYS_PIPE_STATUS_ID, 0, 0, 0, 0); }
+
+int pipeOpen(int pipeID)
+{
+  return _syscall(SYS_PIPE_OPEN_ID, (uint64_t)pipeID, 0, 0, 0);
+}
+
+int pipeClose(int pipeID)
+{
+  return _syscall(SYS_PIPE_CLOSE_ID, (uint64_t)pipeID, 0, 0, 0);
+}
+
+int pipeWrite(int pipeID, char *str)
+{
+  return _syscall(SYS_PIPE_WRITE_ID, (uint64_t)pipeID, (uint64_t)str, 0, 0);
+}
+
+int pipeRead(int pipeID)
+{
+  return _syscall(SYS_PIPE_READ_ID, (uint64_t)pipeID, 0, 0, 0);
+}
+
+void wait(int pipeID)
+{
+  (void)_syscall(SYS_WAIT_ID, (uint64_t)pipeID, 0, 0, 0);
+}
