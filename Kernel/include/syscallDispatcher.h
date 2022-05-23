0b61e3fbe8e764965164ced0bc3324c7ee9a5148
matthewm00 <mateo.menghini@gmail.com>
2022-05-23 00:07:50 -0300

    syscall

--- Kernel/include/syscallDispatcher.h
+++ Kernel/include/syscallDispatcher.h
@@ -6,6 +6,14 @@
 #include <keyboardDriver.h>
 #include <videoDriver.h>
 #include <time.h>
+
+#include <memoryManager.h>
+#include <pipes.h>
+#include <processManager.h>
+#include <processManagerQueue.h>
+#include <semaphores.h>
+#include <syscallDispatcher.h>
+
 uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
 
 #endif
