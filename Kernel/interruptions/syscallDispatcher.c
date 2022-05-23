088ce04db547d4b1675287a7e9a1fac06ad8b2a7
matthewm00 <mateo.menghini@gmail.com>
2022-05-23 16:58:20 -0300

    Co-authored-by: NeksHinto <nhinojo@itba.edu.ar> Co-authored-by: Santiago A. Larroudé Álvarez <SSanti32@users.noreply.github.com> syscallDispatcher init

--- Kernel/interruptions/syscallDispatcher.c
+++ Kernel/interruptions/syscallDispatcher.c
@@ -42,6 +42,73 @@ uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rc
     case SYS_STOPWATCHTICKS_ID:
         return getStopwatchTicks();
         break;
+
+        // nuevos:
+
+    case SYS_MALLOC_ID:
+        return (uint64_t)malloc((size_t)rsi);
+        break;
+    case SYS_FREE_ID:
+        free((void *)rsi);
+        return 0;
+        break;
+    case SYS_MEMORY_STATUS_ID:
+        break;
+    case SYS_NEW_PROCESS_ID:
+        return newProcess((void (*)(int, char **))rsi, (int)rdx, (char **)rcx, (int)r8);
+        break;
+    case SYS_KILL_PROCESS_ID:
+        return killProcess(rsi);
+        break;
+    case SYS_PROCESS_PID_ID:
+        return getProcessPID();
+        break;
+    case SYS_PROCESS_STATUS_ID:
+        printProcessStatus();
+        return 0;
+        break;
+    case SYS_BLOCK_PROCESS_ID:
+        return blockProcess(rsi);
+        break;
+    case SYS_CHANGE_PROCESS_STATE_ID:
+        return setState(rsi, (t_state)rdx);
+        break;
+    case SYS_YIELD_ID:
+        yield();
+        return 0;
+        break;
+    case SYS_SET_PRIORITY_ID:
+        setPriority(rsi, (int)rdx);
+        return 0;
+        break;
+    case SYS_UNBLOCK_PROCESS_ID:
+        return readyProcess(rsi);
+        break;
+    case SYS_SEM_OPEN_ID:
+        break;
+    case SYS_SEM_POST_ID:
+        break;
+    case SYS_SEM_WAIT_ID:
+        break;
+    case SYS_SEM_CLOSE_ID:
+        break;
+    case SYS_SEM_STATUS_ID:
+        break;
+    case SYS_SEC_ELAPSED_ID:
+        break;
+    case SYS_PIPE_STATUS_ID:
+        break;
+    case SYS_PIPE_OPEN_ID:
+        break;
+    case SYS_PIPE_CLOSE_ID:
+        break;
+    case SYS_PIPE_WRITE_ID:
+        break;
+    case SYS_PIPE_READ_ID:
+        break;
+    case SYS_WAIT_ID:
+        break;
+
     default:
         break;
     }
