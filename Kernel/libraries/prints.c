84f8a2ad406d157b4bcb2b71565e1f43c6b3adbb
matthewm00 <mateo.menghini@gmail.com>
2022-05-24 00:54:34 -0300

    prints added

--- Kernel/libraries/prints.c
+++ Kernel/libraries/prints.c
@@ -8,7 +8,7 @@ void printLine()
 	newLine();
 }
 
-static int strlen(char *str)
+int strlen(char *str)
 {
 	int size = 0;
 	for (int i = 0; str[i] != 0; i++)
@@ -111,6 +111,73 @@ void printString(char *string)
 	sys_write(string, strlen(string), BLACK, WHITE);
 }
 
+void swap(char *x, char *y)
+{
+	char t = *x;
+	*x = *y;
+	*y = t;
+}
+
+// function to reverse buffer[i..j]
+char *reverse(char *buffer, int i, int j)
+{
+	while (i < j)
+		swap(&buffer[i++], &buffer[j--]);
+
+	return buffer;
+}
+
+int abs(int num) { return num < 0 ? -num : num; }
+
+// Iterative function to implement itoa() function in C
+char *intToStr(int value, char *buffer, int base)
+{
+	// invalid input
+	if (base < 2 || base > 32)
+	{
+		return buffer;
+	}
+
+	// consider the absolute value of the number
+	int n = abs(value);
+
+	int i = 0;
+	while (n)
+	{
+		int r = n % base;
+
+		if (r >= 10)
+		{
+			buffer[i++] = 65 + (r - 10);
+		}
+		else
+		{
+			buffer[i++] = 48 + r;
+		}
+
+		n = n / base;
+	}
+
+	// if the number is 0
+	if (i == 0)
+	{
+		buffer[i++] = '0';
+	}
+
+	// If the base is 10 and the value is negative, the resulting string
+	// is preceded with a minus sign (-)
+	// With any other base, value is always considered unsigned
+	if (value < 0 && base == 10)
+	{
+		buffer[i++] = '-';
+	}
+
+	buffer[i] = '\0'; // null terminate string
+
+	// reverse the string and return it
+	return reverse(buffer, 0, i - 1);
+}
+
 static int scan(char const *fmt, va_list arg)
 {
 	int int_temp;
@@ -165,72 +232,3 @@ static int scan(char const *fmt, va_list arg)
 	}
 	return length;
 }
-
-// https://www.techiedelight.com/implement-itoa-function-in-c/
-
-// Iterative function to implement itoa() function in C
-char *intToStr(int value, char *buffer, int base)
-{
-	// invalid input
-	if (base < 2 || base > 32)
-	{
-		return buffer;
-	}
-
-	// consider the absolute value of the number
-	int n = abs(value);
-
-	int i = 0;
-	while (n)
-	{
-		int r = n % base;
-
-		if (r >= 10)
-		{
-			buffer[i++] = 65 + (r - 10);
-		}
-		else
-		{
-			buffer[i++] = 48 + r;
-		}
-
-		n = n / base;
-	}
-
-	// if the number is 0
-	if (i == 0)
-	{
-		buffer[i++] = '0';
-	}
-
-	// If the base is 10 and the value is negative, the resulting string
-	// is preceded with a minus sign (-)
-	// With any other base, value is always considered unsigned
-	if (value < 0 && base == 10)
-	{
-		buffer[i++] = '-';
-	}
-
-	buffer[i] = '\0'; // null terminate string
-
-	// reverse the string and return it
-	return reverse(buffer, 0, i - 1);
-}
-
-// function to reverse buffer[i..j]
-char *reverse(char *buffer, int i, int j)
-{
-	while (i < j)
-		swap(&buffer[i++], &buffer[j--]);
-
-	return buffer;
-}
-
-void swap(char *x, char *y)
-{
-	char t = *x;
-	*x = *y;
-	*y = t;
-}
-
-int abs(int num) { return num < 0 ? -num : num; }
