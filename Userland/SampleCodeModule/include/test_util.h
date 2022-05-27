dc4745d9de3717aed335a23ee463738d2b58093f
matthewm00 <mateo.menghini@gmail.com>
2022-05-27 16:32:24 -0300

    test_utils

--- Userland/SampleCodeModule/include/test_util.h
+++ Userland/SampleCodeModule/include/test_util.h
@@ -1,9 +1,13 @@
+#include <stdio.h>
 #include <stdint.h>
+#include <syscalls.h>
+#include <libc.h>
 
 uint32_t GetUint();
 uint32_t GetUniform(uint32_t max);
 uint8_t memcheck(void *start, uint8_t value, uint32_t size);
-int64_t satoi(char* str);
+int64_t satoi(char *str);
 void bussy_wait(uint64_t n);
 void endless_loop();
 void endless_loop_print(uint64_t wait);
+void idleProcess();
