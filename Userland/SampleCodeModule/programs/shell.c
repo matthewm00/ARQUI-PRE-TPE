bd4231e60e688f7838e37ad646c6756b96127c01
matthewm00 <mateo.menghini@gmail.com>
2022-05-27 18:06:03 -0300

    shell dev

--- Userland/SampleCodeModule/programs/shell.c
+++ Userland/SampleCodeModule/programs/shell.c
@@ -9,35 +9,30 @@ void initialize()
     shellExecute();
 }
 
-static int getCommandArgs(char *userInput, char *command, char argv[MAX_ARGUMENTS][BUFFER_SIZE])
+static int getCommandArgs(char *userInput, char **argv)
 {
     int argc = 0;
-    int i = 0;
-    for (i = 0; userInput[i] != 0 && userInput[i] != ' '; i++)
-    {
-        command[i] = userInput[i];
-    }
-    command[i] = 0;
-    int argIdx = 0;
 
-    for (int j = 0; j < MAX_ARGUMENTS; j++)
+    if (*userInput != ' ' && *userInput != '\0')
     {
-        argv[j][0] = 0;
+        argv[argc++] = userInput;
     }
-
-    while (userInput[i] != 0 && argc < MAX_ARGUMENTS)
+    while (*userInput != 0)
     {
-        i++;
-        argIdx = 0;
-        for (; userInput[i] != ' ' && userInput[i] != 0; i++, argIdx++)
+        if (*userInput == ' ')
         {
-            argv[argc][argIdx] = userInput[i];
+            *userInput = 0;
+            if ((*(userInput + 1) != ' ') && (*(userInput + 1) != 0))
+            {
+                if (argc >= MAX_ARGUMENTS)
+                {
+                    return -1;
+                }
+                argv[argc++] = userInput + 1;
+            }
         }
-        argv[argc][argIdx] = 0;
-        argc++;
+        userInput++;
     }
-    if (argc == MAX_ARGUMENTS && userInput[i] != 0)
-        return -1;
     return argc;
 }
 
@@ -52,7 +47,6 @@ t_command inforegg = {&getInfoReg, "a", "aa"};
 
 void shellExecute()
 {
-    char command[BUFFER_SIZE] = {0};
     char argv[MAX_ARGUMENTS][BUFFER_SIZE];
     char userInput[BUFFER_SIZE] = {0};
     int argc = 0;
@@ -66,100 +60,108 @@ void shellExecute()
 
         scanf("%s", userInput);
 
-        argc = getCommandArgs(userInput, command, argv);
+        argc = getCommandArgs(userInput, argv);
 
         if (argc == -1)
         {
             printf("\nIngreso argumentos de mas.\nLa maxima cantidad de argumentos permitida es: %d.\n\n", MAX_ARGUMENTS);
+            continue;
         }
-        else if (strcmp("help", command) == 0)
+
+        if (argv[argc - 1][0] == '&')
+        {
+            foreground = BACKGROUND;
+            argc--;
+        }
+
+        if (strcmp("help", argv[0]) == 0)
         {
             // newProcess((void (*)(int, char **))help, argc, (char **)argv, foreground);
             help(argc, (char **)argv);
         }
-        else if (strcmp("inforeg", command) == 0)
+        else if (strcmp("inforeg", argv[0]) == 0)
         {
             newProcess(&getInfoReg, argc, (char **)argv, foreground, NULL);
             // getInfoReg(argc, (char **)argv);
             //(void (*)(int, char **))
         }
-        else if (strcmp("clear", command) == 0)
+        else if (strcmp("clear", argv[0]) == 0)
         {
             clear(argc, (char **)argv);
         }
-        else if (strcmp("exit", command) == 0)
+        else if (strcmp("exit", argv[0]) == 0)
         {
             exit(argc, (char **)argv);
         }
-        else if (strcmp("opcode", command) == 0)
+        else if (strcmp("opcode", argv[0]) == 0)
         {
             opCode(argc, (char **)argv);
         }
-        else if (strcmp("date", command) == 0)
+        else if (strcmp("date", argv[0]) == 0)
         {
             getCurrentDayTime(argc, (char **)argv);
         }
-        else if (strcmp("printmem", command) == 0)
+        else if (strcmp("printmem", argv[0]) == 0)
         {
             getMem(argc, (char **)argv);
         }
-        else if (strcmp("divzero", command) == 0)
+        else if (strcmp("divzero", argv[0]) == 0)
         {
             divZero(argc, (char **)argv);
         }
-        else if (strcmp("games", command) == 0)
+        else if (strcmp("games", argv[0]) == 0)
         {
             games(argc, (char **)argv);
         }
-        else if (strcmp("mem", command) == 0)
+        else if (strcmp("mem", argv[0]) == 0)
         {
             callMemStatus(argc, (char **)argv);
         }
-        else if (strcmp("sem", command) == 0)
+        else if (strcmp("sem", argv[0]) == 0)
         {
             callSemStatus(argc, (char **)argv);
         }
-        else if (strcmp("ps", command) == 0)
+        else if (strcmp("ps", argv[0]) == 0)
         {
             callProcessStatus(argc, (char **)argv);
         }
-        else if (strcmp("nice", command) == 0)
+        else if (strcmp("nice", argv[0]) == 0)
         {
             callSetPriority(argc, (char **)argv);
         }
-        else if (strcmp("kill", command) == 0)
+        else if (strcmp("kill", argv[0]) == 0)
         {
             callKillProcess(argc, (char **)argv);
         }
-        else if (strcmp("block", command) == 0)
+        else if (strcmp("block", argv[0]) == 0)
         {
             callBlockProcess(argc, (char **)argv);
         }
-        else if (strcmp("unblock", command) == 0)
+        else if (strcmp("unblock", argv[0]) == 0)
         {
             callUnblockProcess(argc, (char **)argv);
         }
-        else if (strcmp("cat", command) == 0)
+        else if (strcmp("cat", argv[0]) == 0)
         {
             cat(argc, (char **)argv);
         }
-        else if (strcmp("loop", command) == 0)
+        else if (strcmp("loop", argv[0]) == 0)
         {
             loop(argc, (char **)argv);
         }
-        else if (strcmp("pipe", command) == 0) // falta
+        else if (strcmp("pipe", argv[0]) == 0) // falta
         {
             // callPipeStatus(argc, (char **)argv);
         }
-        else if (strcmp("filter", command) == 0)
+        else if (strcmp("filter", argv[0]) == 0)
         {
             filter(argc, (char **)argv);
         }
-        else if (strcmp("wc", command) == 0)
+        else if (strcmp("wc", argv[0]) == 0)
         {
             wc(argc, (char **)argv);
         }
-        else if (strcmp("phylo", command) == 0) // falta
+        else if (strcmp("phylo", argv[0]) == 0) // falta
         {
             // phylo(argc, (char **)argv);
         }
